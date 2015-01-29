efbede4105fc3a5b2abb01da917b198ac1b82a63
ebner <simon.ebner@psi.ch>
2015-01-29 11:19:01 +0100

    SFELDAQ-16 - cleaned up variable name bsread as it was overwriting the bsread namespace ...

--- bsread_receiver.py
+++ bsread_receiver.py
@@ -5,7 +5,7 @@ import zmq
 
 if __name__ == "__main__":
     # Startup test sender
-    bsread = bsread.Bsread(mode=zmq.PULL)
-    bsread.connect(address="tcp://localhost:9999", conn_type="connect", )
+    receiver = bsread.Bsread(mode=zmq.PULL)
+    receiver.connect(address="tcp://localhost:9999", conn_type="connect", )
     while True:
-        print bsread.receive()
+        print receiver.receive()
--- bsread_sender.py
+++ bsread_sender.py
@@ -5,6 +5,6 @@ import zmq
 
 if __name__ == "__main__":
     # Startup test sender
-    bsread = bsread.Bsread(mode=zmq.PUSH)
-    bsread.connect(address="tcp://*:9999", conn_type="bind", )
-    bsread.send()
+    sender = bsread.Bsread(mode=zmq.PUSH)
+    sender.connect(address="tcp://*:9999", conn_type="bind", )
+    sender.send()
