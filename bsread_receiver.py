20cc5a6497d8df44375ef9f00a4d5712cb2b7457
ebner <simon.ebner@psi.ch>
2015-02-11 11:59:34 +0100

    SFELDAQ-23 Fixed example client

--- bsread_receiver.py
+++ bsread_receiver.py
@@ -10,10 +10,10 @@ def handle_data_header(header):
 
 if __name__ == "__main__":
     receiver = bsread.Bsread(mode=zmq.PULL)
-    bsread.connect(address="tcp://localhost:9999", conn_type="connect", )
+    receiver.connect(address="tcp://localhost:9999", conn_type="connect", )
 
     while True:
-        message_data = bsread.receive()
+        message_data = receiver.receive()
         if "data_header" in message_data:
             print "Data Header: ", message_data['data_header']
         print message_data['data']
