e9f7492cf56041015b5381800d75715d495593a6
ebner <simon.ebner@psi.ch>
2015-02-18 13:04:58 +0100

    now able to read array_1.0 streams

--- handler/array_1_0.py
+++ handler/array_1_0.py
@@ -5,84 +5,26 @@ import numpy
 class Handler:
 
     def __init__(self):
-        self.header_hash = None
-        self.receive_functions = None
+        return
 
     def receive(self, socket, header):
         return_value = {}
-
         data = []
-        if socket.getsockopt(zmq.RCVMORE) and (not self.header_hash or not self.header_hash == header['hash']):
-            # Interpret data header
-            data_header = socket.recv_json()
-            self.receive_functions = get_receive_functions(data_header)
-            self.header_hash = header['hash']
 
-            return_value['data_header'] = data_header
-        else:
-            # Skip second header
-            socket.recv()
+        # header contains: "htype", "shape", "type", "frame", "endianess", "source", "encoding", "tags"
 
         # Receiving data
-        counter = 0
         while socket.getsockopt(zmq.RCVMORE):
             raw_data = socket.recv()
             if raw_data:
-                data.append(self.receive_functions[counter][1](raw_data))
+                data.append(get_image(raw_data, header['type'], header['shape']))
             else:
                 data.append(None)
-            counter += 1
-
-        # pop the last None value because of the last empty submessage that terminates the message
-        data.pop()
-        # Todo need to add some more error checking
 
         return_value['header'] = header
         return_value['data'] = data
         return return_value
 
 
-# Supporting functions ...
-
-def get_receive_functions(configuration):
-
-    functions = []
-    for channel in configuration['channels']:
-        if channel['type'].lower() == 'double':
-            functions.append((channel, get_double))
-        if channel['type'].lower() == 'integer':
-            functions.append((channel, get_integer))
-        if channel['type'].lower() == 'long':
-            functions.append((channel, get_long))
-        if channel['type'].lower() == 'string':
-            functions.append((channel, get_string))
-
-    return functions
-
-
-def get_double(raw_data):
-    value = numpy.fromstring(raw_data, dtype='f8')
-    if len(value) > 1:
-        return value
-    else:
-        return value[0]
-
-
-def get_integer(raw_data):
-    value = numpy.fromstring(raw_data, dtype='i4')
-    if len(value) > 1:
-        return value
-    else:
-        return value[0]
-
-
-def get_long(raw_data):
-    value = numpy.fromstring(raw_data, dtype='i8')
-    if len(value) > 1:
-        return value
-    else:
-        return value[0]
-
-
-def get_string(raw_data):
-    return raw_data
+def get_image(raw_data, dtype, shape):
+    return numpy.fromstring(raw_data, dtype=dtype, shape=shape)
