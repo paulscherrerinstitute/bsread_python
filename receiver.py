e016b7605c3485cd4870c76ed1c271a85342ad50
ebner <simon.ebner@psi.ch>
2015-02-23 09:59:22 +0100

    SFELDAQ-25 Now supporting additional metadata per channels (and global metadata)
    /pulse_id
    /<channel>/pulse_id
    /<channel>/timestamp
    /<channel>/timestamp_offset

--- receiver.py
+++ receiver.py
@@ -7,8 +7,8 @@ import writer as wr
 
 if __name__ == "__main__":
     receiver = bsread.Bsread(mode=zmq.PULL)
-    # receiver.connect(address="tcp://localhost:9999", conn_type="connect", )
-    receiver.connect(address="tcp://gfa-lc6-64:9999", conn_type="connect", )
+    receiver.connect(address="tcp://localhost:9999", conn_type="connect", )
+    # receiver.connect(address="tcp://gfa-lc6-64:9999", conn_type="connect", )
 
     writer = wr.Writer()
     writer.open_file('test.h5')
@@ -20,6 +20,8 @@ if __name__ == "__main__":
                 data_header = message_data['data_header']
                 print "Data Header: ", data_header
 
+                writer.add_dataset('/pulse_id', dataset_group_name='pulse_id_array', dtype='i8')
+
                 # Interpret the data header and add required datasets
                 for channel in data_header['channels']:
                     dtype = 'f8'
@@ -29,18 +31,33 @@ if __name__ == "__main__":
                         dtype = 'i8'
                     elif channel['type'].lower() == 'string':
                         # we are skipping strings as they are not supported ...
-                        writer.add_dataset_stub()
+                        writer.add_dataset_stub(dataset_group_name='data')
+                        writer.add_dataset_stub(dataset_group_name='timestamp')
+                        writer.add_dataset_stub(dataset_group_name='timestamp_offset')
+                        writer.add_dataset_stub(dataset_group_name='pulse_ids')
                         continue
 
                     if 'shape' in channel:
                         shape = [1] + channel['shape']
                         maxshape = [None] + channel['shape']
-                        writer.add_dataset('/'+channel['name']+'/data', shape=shape, maxshape=maxshape, dtype=dtype)
+                        writer.add_dataset('/'+channel['name']+'/data', dataset_group_name='data', shape=shape, maxshape=maxshape, dtype=dtype)
                     else:
-                        writer.add_dataset('/'+channel['name']+'/data', dtype=dtype)
+                        writer.add_dataset('/'+channel['name']+'/data', dataset_group_name='data', dtype=dtype)
+
+                    # Add new datasets (in different dataset groups) for timestamp, timestamp_offset and pulse_ids
+                    writer.add_dataset('/'+channel['name']+'/timestamp', dataset_group_name='timestamp', dtype='i8')
+                    writer.add_dataset('/'+channel['name']+'/timestamp_offset', dataset_group_name='timestamp_offset', dtype='i8')
+                    writer.add_dataset('/'+channel['name']+'/pulse_id', dataset_group_name='pulse_ids', dtype='i8')
 
             data = message_data['data']
             print data
-            writer.write(data)
+
+            writer.write(message_data['pulse_id_array'], dataset_group_name='pulse_id_array')
+
+            writer.write(data, dataset_group_name='data')
+            writer.write(message_data['timestamp'], dataset_group_name='timestamp')
+            writer.write(message_data['timestamp_offset'], dataset_group_name='timestamp_offset')
+            writer.write(message_data['pulse_ids'], dataset_group_name='pulse_ids')
+
     finally:
         writer.close_file()
