0412f8c7630ef42168baef629bb53be6b5f11be2
ebner <simon.ebner@psi.ch>
2015-04-21 16:04:51 +0200

    updates to the printing ...

--- bsread_receiver.py
+++ bsread_receiver.py
@@ -10,6 +10,10 @@ def receive(source):
 
     while True:
         message_data = receiver.receive()
+
+        # if message_data['header']['pulse_id'] % 10 == 0:
+        #     sys.stderr.write("\x1b[2J\x1b[H")
+
         if "data_header" in message_data:
             print "Data Header: ", message_data['data_header']
         print message_data['data'],  message_data['timestamp'], message_data['header']
@@ -20,6 +24,7 @@ if __name__ == "__main__":
     import getopt
 
     source_ = 'tcp://localhost:9999'  # 'tcp://gfa-lc6-64:9999'
+    source_ = 'tcp://pineapple.psi.ch:51227'
 
     arguments = sys.argv[1:]
     usage = sys.argv[0]+' -s <source>'
