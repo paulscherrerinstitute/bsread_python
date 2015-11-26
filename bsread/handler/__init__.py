7c42b844d6e95d1f8b27b56206e146ad997a9efd
ebner <simon.ebner@psi.ch>
2015-11-26 11:09:57 +0100

    fixed module loading

--- bsread/handler/__init__.py
+++ bsread/handler/__init__.py
@@ -8,7 +8,9 @@ for f in glob.glob(os.path.dirname(__file__) + "/*.py"):
         __all__.append(os.path.basename(f)[:-3])
 
 def load(htype):
+    import importlib
     # Todo: Add some more logic to use an more general handler if possible
     # i.e. htype-1.2 can be handled with an htype_1.py handler
-    s_type = __import__("handler." + htype.replace('.', '_').replace('-', '_'), fromlist=".")
+    s_type = importlib.import_module("."+htype.replace('.', '_').replace('-', '_'), __name__)
+    # s_type = __import__("handler." + htype.replace('.', '_').replace('-', '_'), fromlist=".")
     return s_type.Handler()
