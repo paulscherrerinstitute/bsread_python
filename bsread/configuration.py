b0c47a4ba984fbac18a2159e20d37478c03a5c34
ebner <simon.ebner@psi.ch>
2015-11-30 16:48:59 +0100

    improved script for configuration of IOC

--- bsread/configuration.py
+++ bsread/configuration.py
@@ -1,25 +1,56 @@
-#!/usr/bin/python
 import json
-import os
 import sys
 import re
 import zmq
+import logging
 
-def bsread_config_send(address,req):
-    print("Sending to:",address)
+logger = logging.getLogger(__name__)
+
+
+class Configuration:
+    def __init__(self):
+        self.channels = []
+
+    def json(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
+
+
+class Channel:
+    def __init__(self, name, modulo=None, offset=None):
+        self.name = name
+        if modulo is not None:
+            self.modulo = modulo
+        if offset is not None:
+            self.offset = offset
+
+
+def configure(address, configuration_string):
+    """
+    Configures address with the passed configuration
+    Args:
+        address:        Address to configure tcp://<ioc>:<port>
+        configuration_string:
+
+    Returns: Result of the operation
+    """
+
+    logger.info("Configuring: ", address)
+    logger.info("Configuration: ", configuration_string)
+
     ctx = zmq.Context()
-    sock = zmq.Socket(ctx,zmq.REQ)
+    sock = zmq.Socket(ctx, zmq.REQ)
     sock.connect(address)
 
-    sock.send_string(req)
-    resp = sock.recv_json()
+    sock.send_string(configuration_string)
+    response = sock.recv_json()
 
     sock.close()
     ctx.destroy()
 
-    return resp
+    return response
 
-def configure_ioc(ioc_name):
+
+def read_configuration():
 
     # we use an input_provider class instead of a list of lines as we
     # want to be able to have interactive input as well
@@ -49,28 +80,28 @@ def configure_ioc(ioc_name):
         except ValueError:
             print("modulo (float) or offset (int) specified in wrong type - ignoring channel: "+name)
 
-    configuration_string = configuration.json()
-    print("Configuration String: ")
-    print(configuration_string)
-
-    return bsread_config_send(ioc_name,configuration_string)
+    return configuration.json()
 
 
-class Configuration:
-    def __init__(self):
-        self.channels = []
+def main():
 
-    def json(self):
-        return json.dumps(self, default=lambda o: o.__dict__)
+    import argparse
+    parser = argparse.ArgumentParser(description='BSREAD configuration utility')
+    parser.add_argument('address', type=str, help='Address to configure, has to be in format "tcp://<address>:<port>"')
+    parser.add_argument('-a', '--all', action='count', help='Stream all channels of the IOC')
 
+    arguments = parser.parse_args()
 
-class Channel:
-    def __init__(self, name, modulo=None, offset=None):
-        self.name = name
-        if modulo is not None:
-            self.modulo = modulo
-        if offset is not None:
-            self.offset = offset
+    # Check if to configure all channels
+    if arguments.all:
+        # Sending special JSON to the IOC to configure all channels to be streamed out
+        configuration_string = json.dumps({"grep": 2})
+    else:
+        configuration_string = read_configuration()
+
+    response = configure(arguments.address, configuration_string)
+
+    print(response)
 
 
 if __name__ == '__main__':
@@ -99,14 +130,5 @@ if __name__ == '__main__':
     echo -e "one\ntwo\nthree" | python bsread_client.py
     """
 
-    # ioc = "tcp://localhost:9995"
-    if len(sys.argv) > 1:
-        ioc = sys.argv[1]
-
-    if len(sys.argv) > 2:
-        response = bsread_config_send(ioc,json.dumps({"grep":2}))
-        print(response)
-
-    response = configure_ioc(ioc)
-    print(response)
+    main()
 
