8ffae87b39d840ead6de1abe2ebd2c31185d61ff
ebner <simon.ebner@psi.ch>
2016-01-25 14:30:51 +0100

    fixed sender

--- bsread/bsread.py
+++ bsread/bsread.py
@@ -163,6 +163,8 @@ def get_bytearray(value):
         for v in value:
             message.extend(get_bytearray(v))
         return message
+    elif isinstance(value, bytearray)
+        return value
     else:
         return bytearray()
 
