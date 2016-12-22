b9d57561acaf78f47267cdeca6ec3dcb32ce5bb7
ebner <simon.ebner@psi.ch>
2016-12-22 08:21:41 +0100

    Add --all flag to show details of channel

--- bsread/avail.py
+++ bsread/avail.py
@@ -7,10 +7,12 @@ def main():
     parser = argparse.ArgumentParser(description='Get available beam synchronous channels')
 
     parser.add_argument('pattern', type=str, nargs='?', help='Regex channel pattern')
+    parser.add_argument('-a', '--all', action='store_true', default=False, help='Display all meta information')
 
     arguments = parser.parse_args()
 
     pattern = arguments.pattern
+    print_metadata = arguments.all
 
     if not pattern:
         pattern = '.*'
@@ -20,7 +22,12 @@ def main():
     channels = dispatcher.get_current_channels()
     for channel in channels:
         if re.match(pattern, channel['name']):
-            print(channel['name'])
+            if print_metadata:
+                print("{:50} {} {} {} {} {}".format(channel['name'], channel['type'], channel['shape'],
+                                                          channel['modulo'], channel['offset'], channel['source']))
+                # print(channel)
+            else:
+                print(channel['name'])
 
 if __name__ == "__main__":
     main()
