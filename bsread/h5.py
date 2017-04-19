ae5876ddd1926d5831bee13dc4442b7410dba90e
ebner <simon.ebner@psi.ch>
2017-04-19 09:44:07 +0200

    Catch KeyboardInterrupt exception

--- bsread/h5.py
+++ bsread/h5.py
@@ -166,8 +166,8 @@ def main():
     try:
         receive(address, filename, queue_size=queue_size, mode=mode)
 
-    except(TypeError, AttributeError):
-        # Usually AttributeError is thrown if the receiving is terminated via ctrl+c
+    except KeyboardInterrupt:
+        # KeyboardInterrupt is thrown if the receiving is terminated via ctrl+c
         # As we don't want to see a stacktrace then catch this exception
         pass
     finally:
