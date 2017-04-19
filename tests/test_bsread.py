5201a17b01562027d9bf80b19b22d2c067ec53fe
ebner <simon.ebner@psi.ch>
2017-04-19 10:15:04 +0200

    Add test for timeout

--- tests/test_bsread.py
+++ tests/test_bsread.py
@@ -31,6 +31,26 @@ class TestGenerator(unittest.TestCase):
                 # print(message.data.data['Float64'].value)
         print('done')
 
+    def test_receive_timeout(self):
+        from bsread import source
+        from bsread.sender import sender
+
+        with source(host="localhost", port=9999, receive_timeout=10) as in_stream:
+
+            with sender(queue_size=10) as stream:
+
+                # Send Data
+                # stream.send(one=1, two=2)
+                # stream.send(one=3, two=4)
+
+                # Receive and check data
+                # If timeout is not working - this will hang forever - and therefore the test will fail
+                message = in_stream.receive()
+                if message:
+                    print(message.data.data['one'].value)
+
+                self.assertIsNone(message)
+
 
 if __name__ == '__main__':
     unittest.main()
