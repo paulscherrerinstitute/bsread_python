6f636a1a6619e27216312aa57d51cf53b04f8039
Andrej Babic <andrej.babic@psi.ch>
2017-08-29 10:38:49 +0200

    Improve simulation
    External definition of simulated values and tests.

--- bsread/simulate.py
+++ bsread/simulate.py
@@ -1,46 +1,58 @@
-def generate_stream(port):
+import math
+
+
+def waveform(pulse_id):
+    waveform = []
+    for index in range(0, 30, 1):
+        grad = (3.1415 * index / float(200)) + pulse_id / float(100)
+        waveform.append(math.sin(grad))
+    return waveform
+
+
+def image(pulse_id):
+    image = []
+    for i in range(2):
+        image.append([1.0, 2.0, 3.0, 4.0])
+    return image
+
+
+simulated_channels = [{"name": 'ABC', "function": lambda x: x, "metadata": {'type': 'int32'}},
+                      {"name": 'ABCD', "function": lambda x: x * 10.0},
+                      {"name": 'ABCDF', "function": lambda x: x * 100.0},
+                      {"name": 'XYZ', "function": lambda x: x * 200.0},
+                      {"name": 'XYZW', "function": lambda x: 'hello', "metadata": {'type': 'string'}},
+                      {"name": 'WWW', "function": lambda x: [1.0, 2.0, 3.0, 4.0], "metadata":
+                          {'type': 'float64', 'shape': [4]}
+                       },
+                      {"name": 'WAVE', "function": waveform, "metadata": {'shape': [30]}},
+                      {"name": 'IMAGE', "function": image, "metadata": {'shape': [2, 4]}}]
+
+
+def generate_stream(port, n_messages=None, interval=0.01):
     from .sender import Sender
-    import math
-
-    def waveform(pulse_id):
-        waveform = []
-        for index in range(0, 30, 1):
-            grad = (3.1415*index/float(200))+pulse_id/float(100)
-            waveform.append(math.sin(grad))
-        return waveform
-
-    def image(pulse_id):
-        image = []
-        for i in range(2):
-            # line = []
-            # for index in range(0, 30, 1):
-            #     grad = (3.1415*index/float(200))+pulse_id/float(100)
-            #     line.append(math.sin(grad))
-            # image.append(line)
-            image.append([1.0, 2.0, 3.0, 4.0])
-        return image
 
     generator = Sender(port=port)
-    generator.add_channel('ABC', lambda x: x, metadata={'type': 'int32'})
-    generator.add_channel('ABCD', lambda x: x*10.0)
-    generator.add_channel('ABCDF', lambda x: x*100.0)
-    generator.add_channel('XYZ', lambda x: x*200.0)
-    generator.add_channel('XYZW', lambda x: 'hello', metadata={'type': 'string'})
-    generator.add_channel('WWW', lambda x: [1.0, 2.0, 3.0, 4.0], metadata={'type': 'float64', 'shape': [4]})
-    generator.add_channel('WAVE', waveform, metadata={'shape': [30]})
-    generator.add_channel('IMAGE', image, metadata={'shape': [2, 4]})
-    generator.generate_stream()
+
+    for channel in simulated_channels:
+        generator.add_channel(**channel)
+
+    generator.generate_stream(n_messages=n_messages, interval=interval)
 
 
 def main():
     import argparse
 
     parser = argparse.ArgumentParser(description='bsread simulation utility')
-    parser.add_argument('-p', '--port', type=str, default='9999', help='Port number of stream')
+    parser.add_argument('-p', '--port', type=int, default='9999', help='Port number of stream')
+    parser.add_argument('-n', '--n_messages', type=int, default=None, help="Number of messages to generate."
+                                                                           "None means infinity.")
+    parser.add_argument('-i', '--interval', type=int, default=0.01, help="Interval in seconds between messages."
+                                                                         "Default: 0.01 second.")
 
     arguments = parser.parse_args()
 
-    generate_stream(int(arguments.port))
+    generate_stream(port=arguments.port, n_messages=arguments.n_messages, interval=arguments.interval)
+
 
 if __name__ == "__main__":
     main()
---
+++ tests/test_simulate.py
@@ -0,0 +1,59 @@
+import unittest
+from threading import Thread
+
+from bsread import source, simulate
+from bsread.data.helpers import get_serialization_type
+from bsread.handlers import extended
+
+
+class TestSimulate(unittest.TestCase):
+    def test_generate_stream(self):
+
+        n_of_messages = 5
+        generate_thread = Thread(target=simulate.generate_stream, args=(9999, n_of_messages,))
+        generate_thread.setDaemon(True)
+        generate_thread.start()
+
+        handler = extended.Handler().receive
+
+        with source(host="localhost") as receive_stream:
+            for _ in range(n_of_messages):
+                data = receive_stream.receive(handler=handler)
+                received_channels = data.data["data_header"]["channels"]
+
+                self.assertEqual(len(received_channels), len(simulate.simulated_channels),
+                                 "Not all simulated channels were received.")
+
+                for index, received_channel in enumerate(received_channels):
+                    value = data.data["data"][index]
+
+                    receive_name = received_channel["name"]
+                    receive_type = received_channel.get("type")
+                    receive_shape = received_channel.get("shape")
+
+                    send_name = simulate.simulated_channels[index]["name"]
+                    send_type = simulate.simulated_channels[index].get("metadata", {}).get("type", None)
+                    send_shape = simulate.simulated_channels[index].get("metadata", {}).get("shape", None)
+
+                    self.assertEqual(receive_name, send_name, "Channels are not in same order.")
+                    self.assertEqual(receive_type, send_type)
+                    self.assertEqual(receive_shape, send_shape)
+
+                    # Check if the generated type is the same as the received one.
+                    value_sample = simulate.simulated_channels[index]["function"](0)
+
+                    if isinstance(value_sample, (int, float)):
+                        # Scalars have an empty shape.
+                        self.assertListEqual(list(value.shape), [])
+
+                    elif isinstance(value_sample, str):
+                        self.assertTrue(isinstance(value, str))
+
+                    if not isinstance(value, str):
+                        if send_type:
+                            self.assertEqual(value.dtype, get_serialization_type(send_type))
+
+                        if send_shape:
+                            self.assertListEqual(list(value.shape), send_shape)
+
+        generate_thread.join()
