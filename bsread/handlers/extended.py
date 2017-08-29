7e225ca89c3e16d79d6b1dbb0e2c3b9e4c1347a9
Andrej Babic <andrej.babic@psi.ch>
2017-08-29 10:29:56 +0200

    Improve extended header
    The data header is added to each message. The interpretation of
    the messages is now in line with the compact header.

--- bsread/handlers/extended.py
+++ bsread/handlers/extended.py
@@ -9,7 +9,8 @@ from bsread.data.helpers import get_channel_reader, get_value_reader
 class Handler:
 
     def __init__(self):
-        self.header_hash = None
+        self.data_header_hash = None
+        self.data_header = None
         self.channels_definitions = None
 
     def receive(self, receiver):
@@ -33,7 +34,7 @@ class Handler:
 
         if receiver.has_more() and (self.data_header_hash != header['hash']):
 
-            self.header_hash = header['hash']
+            self.data_header_hash = header['hash']
 
             # Read the data header.
             data_header_bytes = receiver.next()
@@ -69,38 +70,44 @@ class Handler:
             self.channels_definitions = [(channel["name"], channel["encoding"], get_channel_reader(channel))
                                          for channel in data_header['channels']]
 
-            return_value['data_header'] = data_header
+            self.data_header = data_header
         else:
             # Skip second header
             receiver.next()
+
+        # The data header should be added to every message.
+        return_value['data_header'] = self.data_header
 
         # Receiving data
         counter = 0
         # msg_data_size = 0
         while receiver.has_more():
+            channel_name, channel_endianness, channel_reader = self.channels_definitions[counter]
+
             raw_data = receiver.next()
-            # msg_data_size += len(raw_data)
 
             if raw_data:
                 pulse_ids.append(pulse_id)
 
-                channel_name, channel_endianness, channel_reader = self.channels_definitions[counter]
                 data.append(channel_reader(raw_data))
 
                 if receiver.has_more():
+
                     raw_timestamp = receiver.next()
-                    timestamp_array = numpy.frombuffer(raw_timestamp, dtype=channel_endianness+'u8')
-                    # secPastEpoch = value[0]
-                    # nsec = value[1]
-                    timestamp.append(timestamp_array[0])
-                    timestamp_offset.append(timestamp_array[1])
+
+                    if raw_timestamp:
+                        timestamp_array = numpy.frombuffer(raw_timestamp, dtype=channel_endianness + 'u8')
+                        timestamp.append(timestamp_array[0])  # Second past epoch
+                        timestamp_offset.append(timestamp_array[1])  # Nanoseconds offset
             else:
                 if receiver.has_more():
                     receiver.next()  # Read empty timestamp message
+
                 data.append(None)
                 timestamp.append(None)
                 timestamp_offset.append(None)
                 pulse_ids.append(None)
+
             counter += 1
 
         # Todo need to add some more error checking
