2aa400ad00cef177a417cdb8767808c448a3246c
gabadinho <jose.gabadinho@psi.ch>
2018-12-12 14:18:18 +0100

    added bsavail.py: confirms availability of b.s.channels

---
+++ bsread/bsavail.py
@@ -0,0 +1,91 @@
+from . import dispatcher
+from .handlers.compact import Handler
+import re
+import sys
+import time
+from . import Source
+import zmq
+
+def pollStream(pattern, timeout):
+    ch_names  = []
+    ch_values = {}
+    messages  = 0
+    try:
+        channels = dispatcher.get_current_channels()
+        for channel in channels:
+            if re.match(pattern, channel['name']):
+                ch_names.append(channel['name'])
+                ch_values[channel['name']] = None
+
+
+        in_stream = Source(channels=ch_names, queue_size=1, receive_timeout=100)
+        in_stream.connect()
+        in_stream.stream.receiver.socket.setsockopt(zmq.LINGER, 0)
+        #print("Created stream %s\n" % in_stream.stream.address)
+
+        time_start = time.time()
+        try:
+            while True:
+                #message = in_stream.receive(block=False)
+                message = in_stream.receive()
+                messages += 1
+
+                try:                    
+                    for ch_name in message.data.data.keys():
+                        ch_values[ch_name] = message.data.data[ch_name].value
+                except:
+                    pass
+
+                got_all_data = True
+                for ch_nm, ch_vl in ch_values.items():
+                    #got_all_data = got_all_data and ch_vl is not None
+                    if ch_vl is None:
+                        got_all_data = False
+                        break
+                if got_all_data:
+                    time_total = time.time() - time_start
+                    print("\nBeam-synchronous data available after %.1f seconds and %d messages" % (time_total, messages))
+                    break
+                if timeout>0 and (time.time()-time_start)>timeout:
+                    print("\nTimeout, unable to retrieve beam-synchronous data!\nMissing channels:")
+                    for ch_nm, ch_vl in ch_values.items():
+                        if ch_vl is None:
+                            print("\n%s\n" % ch_nm)                    
+                    break
+
+                #time.sleep(interval)
+                ##in_stream.stream.receiver.flush(True)
+                #in_stream.stream.receiver.socket.disconnect(in_stream.stream.address)
+                #in_stream.stream.receiver.socket.connect(in_stream.stream.address)
+ 
+        except KeyboardInterrupt:
+            pass
+
+        try:
+            in_stream.disconnect()
+            dispatcher.remove_stream(in_stream.address)
+        except:
+            pass
+
+    except Exception as e:
+        print('Unable to retrieve channels\nReason:\n' + str(e), file=sys.stderr)
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description='Confirm availability of beam-synchronous channels')
+
+    parser.add_argument('pattern', type=str, nargs='?', help='Regex channel pattern')
+    parser.add_argument('-t', '--timeout', default=0, type=int, help='Timeout in seconds')
+
+    arguments = parser.parse_args()
+
+    pattern = arguments.pattern
+    if not pattern:
+        pattern = '.*'
+    pattern = '.*' + pattern + '.*'
+
+    pollStream(pattern, arguments.timeout)
+
+if __name__ == "__main__":
+    main()
