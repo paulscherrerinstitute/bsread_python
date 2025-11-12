67afdf007035073e0bd82dcd54beaf5757ddfe9c
Sven Augustin <sven.augustin@psi.ch>
2025-11-12 15:27:23 +0100

    renamed: bsread.bsread -> bsread.source (the only thing it contains); also expose Sender from the top level

--- bsread/__init__.py
+++ bsread/__init__.py
@@ -1,2 +1,6 @@
+
 from .consts import CONNECT, BIND, PULL, PUSH, PUB, SUB, BASE_DISPATCHER_URL, DEFAULT_DISPATCHER_URL
-from .bsread import *
+from .source import Source, source
+from .sender import Sender, sender, Channel
+
+
