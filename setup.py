fd74451ae23ebb1003a2c9b2fdf5a00cc4775150
Sven Augustin <sven.augustin@psi.ch>
2025-11-13 23:42:30 +0100

    removed bsread.tables from packages

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     author='Paul Scherrer Institute',
     author_email='daq@psi.ch',
     url='https://git.psi.ch/sf_daq/bsread_python',
-    packages=['bsread', 'bsread.handlers', 'bsread.cli', 'bsread.data', 'bsread.tables'],
+    packages=['bsread', 'bsread.handlers', 'bsread.cli', 'bsread.data'],
     requires=['mflow', 'bitshuffle', 'numpy', 'pyzmq', 'click'],
     zip_safe=False,
 
