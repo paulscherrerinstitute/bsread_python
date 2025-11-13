10e8266477cea70e92a8f2c9a903e70deb3d4c2a
Sven Augustin <sven.augustin@psi.ch>
2025-11-13 23:44:10 +0100

    sorting and cosmetics

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 from setuptools import setup
 
 VERSION = (1, 6, 2)
@@ -14,8 +13,9 @@ setup(
     author='Paul Scherrer Institute',
     author_email='daq@psi.ch',
     url='https://git.psi.ch/sf_daq/bsread_python',
-    packages=['bsread', 'bsread.handlers', 'bsread.cli', 'bsread.data'],
-    requires=['mflow', 'bitshuffle', 'numpy', 'pyzmq', 'click'],
-    zip_safe=False,
-
+    packages=['bsread', 'bsread.cli', 'bsread.data', 'bsread.handlers'],
+    requires=['bitshuffle', 'click', 'mflow', 'numpy', 'pyzmq'],
+    zip_safe=False
 )
+
+
