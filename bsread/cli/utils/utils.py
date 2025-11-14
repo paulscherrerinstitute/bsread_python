d8dea4c9ab1e5bded3b4bf9b0334926b99d61fcd
Sven Augustin <sven.augustin@psi.ch>
2025-11-14 17:49:48 +0100

    use BASE_DISPATCHER_URL and DEFAULT_DISPATCHER_URL consistently

--- bsread/cli/utils/utils.py
+++ bsread/cli/utils/utils.py
@@ -1,5 +1,7 @@
 import re
 
+from bsread import BASE_DISPATCHER_URL, DEFAULT_DISPATCHER_URL
+
 
 def get_base_url(base_url=None, backend=None):
     # Get the correct base url based on the values give for base_url and/or backend
@@ -7,9 +9,9 @@ def get_base_url(base_url=None, backend=None):
     if base_url is not None:
         base_url = base_url
     elif backend is not None:
-        base_url = "https://dispatcher-api.psi.ch/"+backend
+        base_url = BASE_DISPATCHER_URL + "/" + backend
     else:
-        base_url = "https://dispatcher-api.psi.ch/sf-databuffer"
+        base_url = DEFAULT_DISPATCHER_URL
 
     return base_url
 
