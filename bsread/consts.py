7342f2d1e38c173a0b26d8951a8091c7d05de302
Sven Augustin <sven.augustin@psi.ch>
2025-11-14 17:52:28 +0100

    cosmetics

--- bsread/consts.py
+++ bsread/consts.py
@@ -9,8 +9,8 @@ SUB = mflow.SUB
 CONNECT = "connect"
 BIND = "bind"
 
-BASE_DISPATCHER_URL = 'https://dispatcher-api.psi.ch'
-DEFAULT_DISPATCHER_URL = BASE_DISPATCHER_URL+'/sf-databuffer'
+BASE_DISPATCHER_URL = "https://dispatcher-api.psi.ch"
+DEFAULT_DISPATCHER_URL = BASE_DISPATCHER_URL + "/sf-databuffer"
 
 
 
