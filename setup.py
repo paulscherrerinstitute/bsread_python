3771a87bc68acbabdff0f3426381ac9492de0929
Sven Augustin <sven.augustin@psi.ch>
2025-11-16 21:22:37 +0100

    bump version to 1.6.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="bsread",
-    version="1.6.2",
+    version="1.6.3",
     url="https://github.com/paulscherrerinstitute/bsread_python",
     description="bsread for Python",
     author="Paul Scherrer Institute",
