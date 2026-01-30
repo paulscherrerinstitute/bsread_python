58048404b1a7f803fe73ac35424abbd3ac3adc02
gobbo_a <alexandre.gobbo@psi.ch>
2026-01-30 13:50:42 +0100

    Added support to pip package & bump version

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python
-
+import os
 from setuptools import find_packages, setup
 
 
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
-    name="bsread",
-    version="2.0.0",
+    name="psi-bsread",
+    version="2.0.1",
     url="https://github.com/paulscherrerinstitute/bsread_python",
     description="bsread for Python",
     author="Paul Scherrer Institute",
     license="GNU GPLv3",
-    requires=["bitshuffle", "click", "mflow", "numpy", "pyzmq"],
-    packages=find_packages()
+    install_requires=["psi-mflow", "click", "numpy", "pyzmq", "requests", "lz4", "bitshuffle"],
+    packages=find_packages(),
+    long_description=read('Readme.md'),
+    long_description_content_type="text/markdown",
 )
 
 
